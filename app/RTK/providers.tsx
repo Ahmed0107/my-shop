7ad5e88c659be50f3767f17848a11c45368070cb
Ahmed0107 <65514906+Ahmed0107@users.noreply.github.com>
2023-04-08 19:16:47 +0200

    Update providers.tsx

--- app/RTK/providers.tsx
+++ app/RTK/providers.tsx
@@ -8,7 +8,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 let persistor = persistStore(store);
 
-export default function Providers({ children }) {
+export default function Providers({ children }:any) {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>{children}</PersistGate>
